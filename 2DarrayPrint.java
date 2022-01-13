411234275b696307358ebd83d0e9453fb9a83992
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-13 23:45:09 +0530

    Create 2DarrayPrint.java

---
+++ 2DarrayPrint.java
@@ -0,0 +1,28 @@
+package com.basicsinjava;
+
+        import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int column = sc.nextInt();
+
+        int [][] number = new int [row][column];
+
+        for(int i = 0; i<row; i++){
+            for(int j=0; j<column; j++){
+                number[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<row; i++){
+            for(int j = 0; j<column; j++){
+                System.out.print(number[i][j]+" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
