a2809e14c15ca55e3e3ee527d6f980b8f8a82c6a
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-13 23:12:21 +0530

    Create printNumbers.java

---
+++ printNumbers.java
@@ -0,0 +1,17 @@
+package com.basicsinjava;
+
+        import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int number[] = new int [size];        //  We have not mention the numbers.
+
+        for(int i=0; i<size; i++){
+            System.out.println(number [i] );
+        }
+    }
+}
