39466ad4798d594a4d91dcaa085f7547bec62a1d
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-13 23:16:30 +0530

    Update printNumbers.java

--- printNumbers.java
+++ printNumbers.java
@@ -15,3 +15,24 @@ public class Main {
         }
     }
 }
+
+package com.basicsinjava;
+
+        import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int number[] = new int [size];        //  We have mentioned the numbers to take as input.
+
+        for(int i = 0; i<size; i++){
+            number[i] = sc.nextInt();
+        }
+        for(int i=0; i<size; i++){
+            System.out.println(number [i] );
+        }
+    }
+}
