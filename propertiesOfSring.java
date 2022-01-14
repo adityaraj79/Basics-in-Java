96355e094a45b341228983dc9f3ef1c92380ab6d
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-15 00:37:56 +0530

    Update propertiesOfSring.java

--- propertiesOfSring.java
+++ propertiesOfSring.java
@@ -12,16 +12,28 @@ public class Main {
         String lastname ="Stark";
         String fullname = firstname+" "+lastname;
         System.out.println(fullname);
+              //output:Tony Stark
       
       // 2) Length
        
         System.out.println(fullname.length());
+                //output:10
       
       // 3) CharAt
       
          for(int i = 0; i<fullname.length(); i++){
            System.out.println(fullname.charAt(i));
          }  
+                 //output:  T
+                 //         o
+                 //         n
+                 //         y
+                 //           
+                 //         S
+                 //         t
+                 //         a
+                 //         r
+                 //         k   
             
              // 4) Compare
 
@@ -40,12 +52,20 @@ public class Main {
         }else{
             System.out.println("String are not equal");
         }
+              //output:String are equal
             
         //if( name1 == name2 ){
         //    System.out.println("String are equal");
         //}else{                                                 //we can also use this in place of the above
         //    System.out.println("String are not equal");          But it doesn't work in some case .
         //}  
+            
+        // 5) Substring
+
+        String sentence = "My name is Tony Stark";
+        String name = sentence.substring(11,15);
+        System.out.println(name);
+              //output:Tony
 
     }
 }
