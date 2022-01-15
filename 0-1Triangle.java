60a23bc25ef8120d067bbad5377f6944a82ee159
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-15 17:39:59 +0530

    Create 0-1Triangle.java

---
+++ 0-1Triangle.java
@@ -0,0 +1,21 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=i; j++){
+                if((i+j)%2==0){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
