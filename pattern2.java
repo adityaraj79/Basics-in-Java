18458f04c01f982e5e972e0f7f257474cfc69eff
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-15 16:28:14 +0530

    Create pattern2.java

---
+++ pattern2.java
@@ -0,0 +1,17 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
