086e9ae355be402112cc76aad0365230aa72f4a4
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-15 16:12:06 +0530

    Create solidRectangle.java

---
+++ solidRectangle.java
@@ -0,0 +1,19 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
