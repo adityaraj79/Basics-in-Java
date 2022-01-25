a328b8715cc4e891fe4827861ec553418d2767bc
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-26 00:41:48 +0530

    Create wrongCode.java

---
+++ wrongCode.java
@@ -0,0 +1,22 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            int m = sc.nextInt();
+            for(int i = 1; i<=m; i++){
+                for(int j = 1; j<=m-i; j++){
+                    System.out.print("  ");
+                }
+                
+                for(int j = 2; j<=i; j++){
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+        
+    }
+}
